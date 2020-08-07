38d4248fd9070a2e2354ec0ed06a8792cb41d06f
joeythompson1 <025582@students.nccvt.k12.de.us>
2020-08-07 14:12:33 -0400

    added files to branch'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//importing user features
+import UserHome from './components/UserContainer/UserHome';
+
+
+export default class App extends Component{
+
+  render() {
+
+    return(
+
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route exact path='/user' component={UserHome} />
+          </Switch>
+        </div>
+      </Router>
+
+    );
+
+  }
+
 }
-
-export default App;
---
+++ src/components/UserContainer/UserHome.js
@@ -0,0 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from 'react';
+
+export default class UserHome extends Component{
+    render() {
+        return(
+
+            <div className="user-home">
+
+                <div className="user-container">
+
+                    <h1>User Home Feature</h1>
+
+                </div>
+
+            </div>
+
+        );
+    }
+}
