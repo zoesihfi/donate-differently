782567c486993431d88a7d42ed2c18702ecdfd4f
datboidylan <kaiyubi18@outlook.com>
2020-08-11 17:37:24 +0000

    message

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//Importing Donation Features
+import MyDonations from './components/DonationContainer/DonationPage';
 
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <h1>Donation App</h1>
+                <div className="App">
+                    <Switch>
+                        <Route exact path='/' component={MyDonations} />
+                        <Route path='/donation' component={MyDonations} />
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
+}
 export default App;
